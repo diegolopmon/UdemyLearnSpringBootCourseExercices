236947908601d105a6cb573cbeed2e947e418f51
Diego Lopez <diegolopmon@gmail.com>
2017-08-22 20:35:52 +0200

    Create a Controller package

--- Security/src/main/java/com/diegolopmon/springbootcourse/exercices/security/config/SecurityConfig.java
+++ Security/src/main/java/com/diegolopmon/springbootcourse/exercices/security/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -14,7 +16,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureAuth(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
         authenticationManagerBuilder
                 .inMemoryAuthentication()
-                .withUser("inmemory").password("password").roles("admin");
+                .withUser("inmemory").password("password").roles("inmemory")
+        .and()
+                .withUser("diego").password("diegopassword").roles("user")
+        .and()
+                .withUser("admin").password("admin").roles("admin");
     }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.
+            authorizeRequests()
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and();
+            // .formLogin()
+            //    .loginPage("/login")
+            //    .permitAll()
+            //    .and()
+            // .logout()
+            //    .logoutSuccessUrl("/login?logout")
+            //    .permitAll();
+    }
 }
---
+++ Security/src/main/java/com/diegolopmon/springbootcourse/exercices/security/controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.diegolopmon.springbootcourse.exercices.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SecurityController {
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello Security!!!");
+        return "home";
+    }
+
+    @RequestMapping("/admin/portal")
+    public String admin(Model model) {
+        model.addAttribute("message", "Admin portal");
+        return "home";
+    }
+}
