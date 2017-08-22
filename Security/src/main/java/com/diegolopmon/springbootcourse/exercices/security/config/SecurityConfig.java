a211423fefa1088cacce0b893c692cdfa8df1441
Diego Lopez <diegolopmon@gmail.com>
2017-08-22 19:56:25 +0200

    Add some in memory users with roles

--- Security/src/main/java/com/diegolopmon/springbootcourse/exercices/security/config/SecurityConfig.java
+++ Security/src/main/java/com/diegolopmon/springbootcourse/exercices/security/config/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.diegolopmon.springbootcourse.exercices.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
+    @Autowired
+    public void configureAuth(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser("inmemory").password("password").roles("admin");
+    }
+
 }
