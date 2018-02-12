27c18141e7282f5525faf4c78300862cc78e56f4
Diego Lopez <diegolopmon@gmail.com>
2018-02-12 20:37:42 +0100

    Create RestApi domain package

---
+++ RestApi/src/main/java/com/diegolopmon/springbootcourse/exercices/restapi/domain/Author.java
@@ -0,0 +1,58 @@
+package com.diegolopmon.springbootcourse.exercices.restapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @OneToMany( mappedBy = "author" )
+    private List<Book> books;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getPosts() {
+        return books;
+    }
+
+    public void setPosts(List<Book> posts) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
