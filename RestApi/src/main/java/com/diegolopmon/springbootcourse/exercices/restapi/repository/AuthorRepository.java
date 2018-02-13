49a26e078fd1e6c8e55fcd2a0c0f38a1ca239fb5
Diego Lopez <diegolopmon@gmail.com>
2018-02-13 21:39:59 +0100

    Fix RestApi repositories

--- RestApi/src/main/java/com/diegolopmon/springbootcourse/exercices/restapi/repository/AuthorRepository.java
+++ RestApi/src/main/java/com/diegolopmon/springbootcourse/exercices/restapi/repository/AuthorRepository.java
@@ -3,10 +3,5 @@ package com.diegolopmon.springbootcourse.exercices.restapi.repository;
 import com.diegolopmon.springbootcourse.exercices.restapi.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface AuthorRepository extends CrudRepository<Author, Long> {
-    Author findFirstByAuthorByFirstNameAndLastName();
-    Author findAuthorById();
-    List<Author> findAll();
 }
--- RestApi/src/main/java/com/diegolopmon/springbootcourse/exercices/restapi/repository/BookRepository.java
+++ RestApi/src/main/java/com/diegolopmon/springbootcourse/exercices/restapi/repository/BookRepository.java
@@ -4,6 +4,4 @@ import com.diegolopmon.springbootcourse.exercices.restapi.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-    Book findFirstByName();
-    Book findAllByAuthor();
 }
