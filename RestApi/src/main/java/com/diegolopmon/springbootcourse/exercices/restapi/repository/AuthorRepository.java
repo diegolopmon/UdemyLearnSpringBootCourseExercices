08b893b2666a7773cccf071b7b420b1b464cdc20
Diego Lopez <diegolopmon@gmail.com>
2018-06-21 23:01:03 +0200

    Spring Data Rest finished

--- RestApi/src/main/java/com/diegolopmon/springbootcourse/exercices/restapi/repository/AuthorRepository.java
+++ RestApi/src/main/java/com/diegolopmon/springbootcourse/exercices/restapi/repository/AuthorRepository.java
@@ -1,7 +1,11 @@
 package com.diegolopmon.springbootcourse.exercices.restapi.repository;
 
 import com.diegolopmon.springbootcourse.exercices.restapi.domain.Author;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+import java.util.List;
+
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+    List<Author> findByFirstNameContaining(@Param("firstName") String firstName);
 }
